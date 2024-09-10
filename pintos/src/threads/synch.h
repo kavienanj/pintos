226c4853c3d1cbe5d13de90399dda77ff1e1118c
ovindu-a <ovindu.atukorala@gmail.com>
2024-09-10 14:50:48 +0530

    Alarm works

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -18,7 +18,7 @@ void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
 void sema_up (struct semaphore *);
 void sema_self_test (void);
-bool sema_compare_priority(const struct list_elem *l, const struct list_elem *s, void *aux);
+// bool sema_compare_priority(const struct list_elem *l, const struct list_elem *s, void *aux);
 /* Lock. */
 struct lock 
   {
